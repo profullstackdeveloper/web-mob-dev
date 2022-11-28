051067104732b0f463e57ae92d2d9811b149303e
profullstackdeveloper <neverdream9106@gmail.com>
2022-11-28 09:59:27 -0500

    update

---
+++ precache-manifest.9e16e9deeeb3641918017cf5ee7e49d2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704fb281d3cf7f1415ad97c8c331eef0",
+    "url": "/web-mob-dev/index.html"
+  },
+  {
+    "revision": "505b88ef768bbc6dac7b",
+    "url": "/web-mob-dev/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "787124d3234e95a82265",
+    "url": "/web-mob-dev/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "3e6d288e6dd3ef69aa9b",
+    "url": "/web-mob-dev/static/css/main.34511000.chunk.css"
+  },
+  {
+    "revision": "b9059c652bf9c049d6c6",
+    "url": "/web-mob-dev/static/js/2.062fa7f6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/web-mob-dev/static/js/2.062fa7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505b88ef768bbc6dac7b",
+    "url": "/web-mob-dev/static/js/3.3a89cf1d.chunk.js"
+  },
+  {
+    "revision": "787124d3234e95a82265",
+    "url": "/web-mob-dev/static/js/4.ce106a27.chunk.js"
+  },
+  {
+    "revision": "3e6d288e6dd3ef69aa9b",
+    "url": "/web-mob-dev/static/js/main.55ff4b62.chunk.js"
+  },
+  {
+    "revision": "feac10e6cd9fe55dfd0a",
+    "url": "/web-mob-dev/static/js/runtime-main.d67bcc47.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/web-mob-dev/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7d37cfd1a7a506105359b81f93468007",
+    "url": "/web-mob-dev/static/media/HKUST.7d37cfd1.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/web-mob-dev/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f86cf5c34c45ba7c8edc52602eaf2c93",
+    "url": "/web-mob-dev/static/media/applied.f86cf5c3.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/web-mob-dev/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/web-mob-dev/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/web-mob-dev/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/web-mob-dev/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "335c6e12e1fdd9fca1411e7a9204f3dd",
+    "url": "/web-mob-dev/static/media/housetable.335c6e12.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/web-mob-dev/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "94e74e062d5a27f43e30c1bd5cd0a17e",
+    "url": "/web-mob-dev/static/media/monsta.94e74e06.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/web-mob-dev/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/web-mob-dev/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/web-mob-dev/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
